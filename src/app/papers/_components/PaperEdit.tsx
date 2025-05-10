35f14e704d5f0223a6f014dfa0bf3156f844dec2
oneieo <sun020716@gmail.com>
2025-05-10 16:02:30 +0900

    chore: 에디터에 마크다운 텍스트 임포트 완료

--- src/app/papers/_components/PaperEdit.tsx
+++ src/app/papers/_components/PaperEdit.tsx
@@ -4,7 +4,7 @@ import NotionEditor from "./NotionEditor";
 import ExtractedContent from "./ExtractedContent";
 import { apiUrl } from "@/app/(auth)/_components/Login";
 import { markdown } from "@yoopta/exports";
-import { getCookie } from "@/app/(main)/_components/Home";
+import { getCookie } from "@/app/utils/getCookie";
 
 interface PaperEditProps {
   summaryId: string;
@@ -18,34 +18,34 @@ const PaperEdit = ({ summaryId }: PaperEditProps) => {
   const hasRun = useRef(false);
   console.log(summaryId);
 
-  useEffect(() => {
-    const fetchSummaryData = async () => {
-      try {
-        const response = await fetch(`${apiUrl}/api/summaries/3/edit`, {
-          method: "GET",
-          headers: {
-            Authorization: `Bearer ${getCookie("accessToken")}`,
-          },
-          credentials: "include",
-        });
+  // useEffect(() => {
+  //   const fetchSummaryData = async () => {
+  //     try {
+  //       const response = await fetch(`${apiUrl}/api/summaries/3/edit`, {
+  //         method: "GET",
+  //         headers: {
+  //           Authorization: `Bearer ${getCookie("accessToken")}`,
+  //         },
+  //         credentials: "include",
+  //       });
 
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
+  //       if (!response.ok) {
+  //         throw new Error(`HTTP error! Status: ${response.status}`);
+  //       }
 
-        const data = await response.json();
-        setSummaryData(data);
-        console.log("요약 데이터 불러오기 성공:", data);
-        return data;
-      } catch (error) {
-        console.error("요약 수정 데이터 가져오기 실패: ", error);
-        throw error;
-      }
-    };
+  //       const data = await response.json();
+  //       setSummaryData(data);
+  //       console.log("요약 데이터 불러오기 성공:", data);
+  //       return data;
+  //     } catch (error) {
+  //       console.error("요약 수정 데이터 가져오기 실패: ", error);
+  //       throw error;
+  //     }
+  //   };
 
-    hasRun.current = true;
-    fetchSummaryData();
-  }, []);
+  //   hasRun.current = true;
+  //   fetchSummaryData();
+  // }, []);
 
   return (
     <div className="flex justify-center items-start pb-11 gap-4">
