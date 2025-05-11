bc90de69a52531e814a1d4d41c4dcf8fc7a30f7a
oneieo <sun020716@gmail.com>
2025-05-11 16:55:25 +0900

    feat: 요약본 임시저장/저장 및 게시글 조회 페이지 구현

---
+++ src/app/papers/published/_components/PublishedLayout.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const PublishedLayout = () => {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <div>
+        <h1 className="text-3xl font-bold mt-8">
+          {/**사용자 이름's paper로 수정 필요 */}
+          oneieo&apos;s paper
+        </h1>
+        <div className="w-[80rem] h-px bg-[#000000] my-8" />
+      </div>
+    </div>
+  );
+};
+
+export default PublishedLayout;
