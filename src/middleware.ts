ef6c21cb276594e9d17283141ed1a19335ee89a8
oneieo <sun020716@gmail.com>
2025-05-24 23:11:21 +0900

    feat: 프로필 등록 기능 구현

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,12 +2,8 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  // "/" 경로에 접근할 때만 체크
   if (request.nextUrl.pathname === "/") {
-    // 쿠키나 헤더에서 사용자 정보 확인
-    const userInfo = request.cookies.get("userInfo");
-
-    // userStore 정보가 없거나 불완전한 경우
+    const userInfo = request.cookies.get("userInfo-storage");
     if (!userInfo || !isUserInfoComplete(userInfo.value)) {
       return NextResponse.redirect(new URL("/sign-up", request.url));
     }
@@ -19,8 +15,7 @@ export function middleware(request: NextRequest) {
 function isUserInfoComplete(userInfoString: string) {
   try {
     const userInfo = JSON.parse(userInfoString);
-    // 필요한 필드들이 모두 있는지 체크
-    return userInfo.name && userInfo.email && userInfo.additionalField;
+    return userInfo.username && userInfo.profileImageUrl && userInfo.interests;
   } catch {
     return false;
   }
