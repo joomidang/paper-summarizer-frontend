31523a43d98f566dfae438b8bbb06304fff19425
oneieo <sun020716@gmail.com>
2025-06-05 13:48:36 +0900

    chore: Home.tsx 리다이렉트 코드 수정

--- src/app/(main)/_components/Home.tsx
+++ src/app/(main)/_components/Home.tsx
@@ -1,16 +1,15 @@
 "use client";
 import { getCookie } from "@/app/utils/getCookie";
 import { useAuthStore } from "@/store/authStore";
-import { useUserInfoStore } from "@/store/userInfoStore";
 import { useRouter } from "next/navigation";
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 
 const Home = () => {
   const { setAccessToken } = useAuthStore();
   const router = useRouter();
-  const { userInfo } = useUserInfoStore();
-  const [isChecking, setIsChecking] = useState(true);
-  const [hasHydrated, setHasHydrated] = useState(false);
+  // const { data: userInfo } = useUserInfo();
+  // const [isChecking] = useState(true);
+  // const [hasHydrated] = useState(false);
 
   useEffect(() => {
     const accessTokenFromCookie = getCookie("accessToken");
@@ -18,57 +17,59 @@ const Home = () => {
       setAccessToken(accessTokenFromCookie);
     } else {
       console.log("No access token found in cookies");
+      router.push("/login");
     }
-  }, [setAccessToken]);
+  }, [setAccessToken, router]);
 
-  useEffect(() => {
-    const unsubscribe = useUserInfoStore.persist.onFinishHydration(() => {
-      setHasHydrated(true);
-    });
+  // useEffect(() => {
+  //   const unsubscribe = useUserInfoStore.persist.onFinishHydration(() => {
+  //     setHasHydrated(true);
+  //   });
 
-    // 이미 하이드레이션이 완료된 경우
-    if (useUserInfoStore.persist.hasHydrated()) {
-      setHasHydrated(true);
-    }
+  //   // 이미 하이드레이션이 완료된 경우
+  //   if (useUserInfoStore.persist.hasHydrated()) {
+  //     setHasHydrated(true);
+  //   }
 
-    return unsubscribe;
-  }, []);
+  //   return unsubscribe;
+  // }, []);
 
-  useEffect(() => {
-    if (!hasHydrated) return;
-    console.log("userInfo:", userInfo);
+  // useEffect(() => {
+  //   if (!hasHydrated) return;
+  //   console.log("userInfo:", userInfo);
 
-    const checkUserInfo = () => {
-      if (
-        !userInfo ||
-        (!userInfo.username &&
-          !userInfo.profileImageUrl &&
-          userInfo.interests.length === 0)
-      ) {
-        router.push("/login");
-        return;
-      }
-      setIsChecking(false);
-    };
+  //   const checkUserInfo = () => {
+  //     console.log("userInfo:", userInfo);
+  //     if (
+  //       !userInfo ||
+  //       (!userInfo.username &&
+  //         !userInfo.profileImageUrl &&
+  //         userInfo.interests.length === 0)
+  //     ) {
+  //       router.push("/login");
+  //       return;
+  //     }
+  //     setIsChecking(false);
+  //   };
 
-    checkUserInfo();
-  }, [userInfo, router, hasHydrated]);
+  //   checkUserInfo();
+  // }, [userInfo, router, hasHydrated]);
 
-  if (!hasHydrated || isChecking) {
-    return (
-      <div className="flex items-center justify-center min-h-screen">
-        <div>Loading...</div>
-      </div>
-    );
-  }
+  // if (!hasHydrated || isChecking) {
+  //   return (
+  //     <div className="flex items-center justify-center min-h-screen">
+  //       <div>Loading...</div>
+  //     </div>
+  //   );
+  // }
 
-  if (isChecking) {
-    return (
-      <div className="flex items-center justify-center min-h-screen">
-        <div>Loading...</div>
-      </div>
-    );
-  }
+  // if (isChecking) {
+  //   return (
+  //     <div className="flex items-center justify-center min-h-screen">
+  //       <div>Loading...</div>
+  //     </div>
+  //   );
+  // }
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
